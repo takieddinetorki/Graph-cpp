f4d3ab399d5a967d86c35da30440b092659f4102
Taki Eddine TORKI <66402797+takieddinetorki@users.noreply.github.com>
2024-02-07 10:35:40 +0100

    Update Source.cpp

--- Graph library/Source.cpp
+++ Graph library/Source.cpp
@@ -5,5 +5,14 @@
 int main()
 {
 	heterogeneous::Graph<std::tuple<int, float>, int> s;
-	s.addNode<std::tuple<int, float>, std::string, char, char, int>("Whatever this is", 'a', 'b', 0);
+
+	for (size_t i = 0.; i < 10; i++)
+	{
+		s.addNode<std::tuple<int, float>, std::string, char, char, int>("Whatever this is", 'a', 'b', i);
+	}
+
+
+
+
+	system("pause");
 }
